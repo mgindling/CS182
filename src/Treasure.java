ec5245776c471591564e7f01639c592c68e4d004
Mark Gindling <mgindlin@purdue.edu>
2018-02-26 10:09:32 -0500

    Worked on Part 2 (the loop part)

--- src/Treasure.java
+++ src/Treasure.java
@@ -13,21 +13,20 @@ import java.util.ArrayList;
 public class Treasure {
     public static int findMaxGold(TreeNode root) {
 
-        //This variable holds the max gold totally found.
+        // This variable holds the max gold found throughout a certain node's children.
         int max = 0;
-        //This variable holds the gold from a certain child.
+        // This variable holds the gold from a certain child.
         int goldGet = 0;
-
-        //Copies the children of a certain node so I can do stuff with it.
-        ArrayList<TreeNode> childList = root.getChildren();
-
-        //Gets the number of children the root we've received has.
-        int childs = childList.size();
-
-        //Used to loop recursively and also to set the max gold correctly.
+        // Used to loop recursively and also to set the max gold correctly.
         int c;
 
-        //Goes down each child path and finds the max gold..
+        // Copies the children of a certain node so I can do stuff with it.
+        ArrayList<TreeNode> childList = root.getChildren();
+
+        // Gets the number of children the root we've received has.
+        int childs = childList.size();
+
+        // Goes down each child path and finds the max gold throughout them all.
         for (c = 0; c < childs; c++) {
             goldGet = findMaxGold(childList.get(c));
             if (goldGet > max) {
@@ -36,6 +35,7 @@ public class Treasure {
             }
         }
 
+        // Returns the right amount of maximum gold found in a certain node. The c == 0 case is the base case.
         if (c > 0) {
             return root.getMaxGold() + root.getGold();
         }
@@ -47,8 +47,43 @@ public class Treasure {
 
     public static ArrayList<TreeNode> findMaxGoldPath(TreeNode root) {
 
-        /*
-        // Starts with the root--prints out the data.
+        // Holds the path to the maximum gold.
+        ArrayList<TreeNode> returnNodes = new ArrayList<>();
+
+        // Starts with the root.
+        returnNodes.add(root);
+
+        //Gets the children and the size of the child array
+        ArrayList<TreeNode> childList = root.getChildren();
+        //Gets the size of the children array
+        int childs = childList.size();
+
+        while (childs > 0) {
+
+            // Holds the maximum gold of the children.
+            int max = 0;
+            // Holds where the maximum gold of the children has been found.
+            int where = 0;
+
+            // Iterates through the child array to find the max max gold.
+            for (int c = 0; c < childs; c++) {
+                if (childList.get(c).getMaxGold() > max) {
+                    where = c;
+                }
+            }
+
+            // Puts the correct node in the array list.
+            returnNodes.add(childList.get(where));
+
+            // Goes into the added child and looks at its children.
+            childList = childList.get(where).getChildren();
+            childs = childList.size();
+
+        }
+
+
+        /* THIS IS A MESS LOL DON'T LOOK PLZ
+        // Starts with the root.
         System.out.println(root.toString());
 
         // Make sure to have a loop and not make it recursive
